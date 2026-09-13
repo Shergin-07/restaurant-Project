30b92a4036a1312f0f4b65938265d308ab9272ba
Shergin-07 <sherginv2006@gmail.com>
2026-09-14 00:21:59 +0530

    In header View button was updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Header.css'
-import { href } from 'react-router-dom'
+
 
 const Header = () => {
+  const handleViewMenu = () => { const menuSection = document.getElementById('explore-menu') 
+    if (menuSection) { menuSection.scrollIntoView({ behavior: 'smooth' }) } }
   return (
     <div className='header'>
      <div className="header-contents">
         <h2>Order you favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise, one delicious meal at a time.</p>
-        <button>View Menu</button>
+        <button onClick={handleViewMenu}>View Menu</button>
      </div>
 
     </div>
